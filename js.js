3bf3de1253c57840cc21e69cb143fc50e559e927
DerickLFAndrade <derickspy15@gmail.com>
2021-10-06 01:47:05 -0300

    Implementação de funções

--- js.js
+++ js.js
@@ -24,9 +24,9 @@ var container2 = document.getElementById('containerSecundario')
 var tema1 = document.getElementById('dark')
 var tema2 = document.getElementById('white')
 
-tema1.addEventListener('click', temaDark) 
+tema1.addEventListener('click', temaDark)
 tema2.addEventListener('click', temaWhite)
-function temaDark () { 
+function temaDark() {
   ativ.style.background = 'black'
   ativ.style.color = 'white'
   container.style.background = 'black'
@@ -37,7 +37,7 @@ function temaDark () {
   resetar.style.color = 'white'
   botão.style.background = 'black'
   botão.style.color = 'white'
- }
+}
 function temaWhite() {
   ativ.style.background = 'white'
   ativ.style.color = 'black'
@@ -77,276 +77,363 @@ function limpar() {
 
 
 function adicionar() {
-if (ativ.value == '') {
-  alert('Para cadastrar uma atividade, preencha o campo "Atividade" abaixo e clique em "Cadastrar"')
-} else {
-  var botdel = document.createElement('button')
-  var botEdt = document.createElement('button')
-  if (op2.innerHTML == '' && op3.innerHTML == '' && op4.innerHTML == '' && op5.innerHTML == '' && op6.innerHTML == '' && op7.innerHTML == '' && op8.innerHTML == '') {
-
-    op1.innerHTML = `1 - ${ativ.value}`
-    op2.innerHTML = '2'
-    ativ.value = ''
-    ativ.placeHolder = 'Atividade'
-    ativ.focus
+  if (ativ.value == '') {
+    alert('Para cadastrar uma atividade, preencha o campo "Atividade" abaixo e clique em "Cadastrar"')
+  } else {
     var botdel = document.createElement('button')
     var botEdt = document.createElement('button')
-    botdel.innerHTML = 'X'
-    botdel.style.position = 'absolute'
-    botdel.style.left = '35vw'
-    botdel.style.background = 'red'
-    botdel.style.borderRadius = '5px'
-    botdel.style.color = 'black'
-    botdel.style.fontWeight = 'bold'
-    botdel.style.fontSize = '1.3vw'
+    if (op2.innerHTML == '' && op3.innerHTML == '' && op4.innerHTML == '' && op5.innerHTML == '' && op6.innerHTML == '' && op7.innerHTML == '' && op8.innerHTML == '') {
 
-    op1.appendChild(botdel)
+      op1.innerHTML = `- ${ativ.value}`
+      op2.innerHTML = '-'
+      ativ.value = ''
+      ativ.placeHolder = 'Atividade'
+      ativ.focus
+      var botdel = document.createElement('button')
+      var botEdt = document.createElement('button')
+      botdel.innerHTML = 'X'
+      botdel.style.position = 'absolute'
+      botdel.style.left = '35vw'
+      botdel.style.background = 'red'
+      botdel.style.borderRadius = '5px'
+      botdel.style.color = 'black'
+      botdel.style.fontWeight = 'bold'
+      botdel.style.fontSize = '1.3vw'
 
-    botEdt.innerHTML = '✏️'
-    botEdt.style.position = 'absolute'
-    botEdt.style.left = '38vw'
-    botEdt.style.background = 'w'
-    botEdt.style.borderRadius = '5px'
-    botEdt.style.color = 'black'
-    botEdt.style.fontWeight = 'bold'
-    botEdt.style.fontSize = '1.3vw'
-
-    op1.appendChild(botEdt)
-   
-    
-
-  } else if (op3.innerHTML == '' && op4.innerHTML == '' && op5.innerHTML == '' && op6.innerHTML == '' && op7.innerHTML == '' && op8.innerHTML == '') {
-
-    op2.innerHTML = `2 - ${ativ.value}`
-    op3.innerHTML = '3 '
-    ativ.value = ''
-    ativ.placeholder = 'Atividade'
-    ativ.focus
+      botdel.addEventListener('click', function () {
+        op1.remove(op1)
+        op1d.remove(op1d)
+        botão.style.visibility = 'visible'
+        ativ.value = ''
+        ativ.placeholder = 'Atividade'
+        ativ.disabled = false
+        botão.disabled = false
 
 
-    var botdel = document.createElement('button')
-    botdel.innerHTML = 'X'
-    botdel.style.position = 'absolute'
-    botdel.style.left = '35vw'
-    botdel.style.background = 'red'
-    botdel.style.borderRadius = '5px'
-    botdel.style.color = 'black'
-    botdel.style.fontWeight = 'bold'
-    botdel.style.fontSize = '1.3vw'
+      })
 
-    op2.appendChild(botdel)
+      op1.appendChild(botdel)
 
-    botEdt.innerHTML = '✏️'
-    botEdt.style.position = 'absolute'
-    botEdt.style.left = '38vw'
-    botEdt.style.background = 'w'
-    botEdt.style.borderRadius = '5px'
-    botEdt.style.color = 'black'
-    botEdt.style.fontWeight = 'bold'
-    botEdt.style.fontSize = '1.3vw'
+      botEdt.innerHTML = '✏️'
+      botEdt.style.position = 'absolute'
+      botEdt.style.left = '38vw'
+      botEdt.style.background = 'w'
+      botEdt.style.borderRadius = '5px'
+      botEdt.style.color = 'black'
+      botEdt.style.fontWeight = 'bold'
+      botEdt.style.fontSize = '1.3vw'
 
-    op2.appendChild(botEdt)
+      op1.appendChild(botEdt)
 
-  } else if (op4.innerHTML == '' && op5.innerHTML == '' && op6.innerHTML == '' && op7.innerHTML == '' && op8.innerHTML == '') {
 
-    op3.innerHTML = `3 - ${ativ.value}`
-    op4.innerHTML = '4 '
-    ativ.value = ''
-    ativ.placeholder = 'Atividade'
-    ativ.focus
 
-    var botdel = document.createElement('button')
-    botdel.innerHTML = 'X'
-   
-    botdel.style.position = 'absolute'
-    botdel.style.left = '35vw'
-    botdel.style.background = 'red'
-    botdel.style.borderRadius = '5px'
-    botdel.style.color = 'black'
-    botdel.style.fontWeight = 'bold'
-    botdel.style.fontSize = '1.3vw'
+    } else if (op3.innerHTML == '' && op4.innerHTML == '' && op5.innerHTML == '' && op6.innerHTML == '' && op7.innerHTML == '' && op8.innerHTML == '') {
 
-    op3.appendChild(botdel)
+      op2.innerHTML = `- ${ativ.value}`
+      op2.style.marginTop = '3vw'
+      op3.innerHTML = '- '
+      ativ.value = ''
+      ativ.placeholder = 'Atividade'
+      ativ.focus
 
-    botEdt.innerHTML = '✏️'
-    botEdt.style.position = 'absolute'
-    botEdt.style.left = '38vw'
-    botEdt.style.background = 'w'
-    botEdt.style.borderRadius = '5px'
-    botEdt.style.color = 'black'
-    botEdt.style.fontWeight = 'bold'
-    botEdt.style.fontSize = '1.3vw'
 
-    op3.appendChild(botEdt)
+      var botdel = document.createElement('button')
+      botdel.innerHTML = 'X'
+      botdel.style.position = 'absolute'
+      botdel.style.left = '35vw'
+      botdel.style.background = 'red'
+      botdel.style.borderRadius = '5px'
+      botdel.style.color = 'black'
+      botdel.style.fontWeight = 'bold'
+      botdel.style.fontSize = '1.3vw'
 
-  } else if (op5.innerHTML == '' && op6.innerHTML == '' && op7.innerHTML == '' && op8.innerHTML == '') {
+      botdel.addEventListener('click', function () {
+        op2.remove(op2)
+        op2d.remove(op2d)
+        botão.style.visibility = 'visible'
+        ativ.value = ''
+        ativ.placeholder = 'Atividade'
+        ativ.disabled = false
+        botão.disabled = false
+      })
 
-    op4.innerHTML = `4 - ${ativ.value}`
-    op5.innerHTML = '5 '
-    ativ.value = ''
-    ativ.placeholder = 'Atividade'
-    ativ.focus
+      op2.appendChild(botdel)
 
-    var botdel = document.createElement('button')
-    botdel.innerHTML = 'X'
-   
-    botdel.style.position = 'absolute'
-    botdel.style.left = '35vw'
-    botdel.style.background = 'red'
-    botdel.style.borderRadius = '5px'
-    botdel.style.color = 'black'
-    botdel.style.fontWeight = 'bold'
-    botdel.style.fontSize = '1.3vw'
+      botEdt.innerHTML = '✏️'
+      botEdt.style.position = 'absolute'
+      botEdt.style.left = '38vw'
+      botEdt.style.background = 'w'
+      botEdt.style.borderRadius = '5px'
+      botEdt.style.color = 'black'
+      botEdt.style.fontWeight = 'bold'
+      botEdt.style.fontSize = '1.3vw'
 
-    op4.appendChild(botdel)
+      op2.appendChild(botEdt)
 
-    botEdt.innerHTML = '✏️'
-    botEdt.style.position = 'absolute'
-    botEdt.style.left = '38vw'
-    botEdt.style.background = 'w'
-    botEdt.style.borderRadius = '5px'
-    botEdt.style.color = 'black'
-    botEdt.style.fontWeight = 'bold'
-    botEdt.style.fontSize = '1.3vw'
+    } else if (op4.innerHTML == '' && op5.innerHTML == '' && op6.innerHTML == '' && op7.innerHTML == '' && op8.innerHTML == '') {
 
-    op4.appendChild(botEdt)
+      op3.innerHTML = `- ${ativ.value}`
+      op3.style.marginTop = '5.2vw'
+      op4.innerHTML = '-'
+      ativ.value = ''
+      ativ.placeholder = 'Atividade'
+      ativ.focus
 
-  } else if (op6.innerHTML == '' && op7.innerHTML == '' && op8.innerHTML == '') {
+      var botdel = document.createElement('button')
+      botdel.innerHTML = 'X'
 
-    op5.innerHTML = `5 - ${ativ.value}`
-    op6.innerHTML = '6 '
-    ativ.value = ''
-    ativ.placeholder = 'Atividade'
-    ativ.focus
+      botdel.style.position = 'absolute'
+      botdel.style.left = '35vw'
+      botdel.style.background = 'red'
+      botdel.style.borderRadius = '5px'
+      botdel.style.color = 'black'
+      botdel.style.fontWeight = 'bold'
+      botdel.style.fontSize = '1.3vw'
 
-    var botdel = document.createElement('button')
-    botdel.innerHTML = 'X'
+      botdel.addEventListener('click', function () {
+        op3.remove(op3)
+        op3d.remove(op3d)
+        botão.style.visibility = 'visible'
+        ativ.value = ''
+        ativ.placeholder = 'Atividade'
+        ativ.disabled = false
+        botão.disabled = false
+      })
 
-    
-    botdel.style.position = 'absolute'
-    botdel.style.left = '35vw'
-    botdel.style.background = 'red'
-    botdel.style.borderRadius = '5px'
-    botdel.style.color = 'black'
-    botdel.style.fontWeight = 'bold'
-    botdel.style.fontSize = '1.3vw'
+      op3.appendChild(botdel)
 
-    op5.appendChild(botdel)
+      botEdt.innerHTML = '✏️'
+      botEdt.style.position = 'absolute'
+      botEdt.style.left = '38vw'
+      botEdt.style.background = 'w'
+      botEdt.style.borderRadius = '5px'
+      botEdt.style.color = 'black'
+      botEdt.style.fontWeight = 'bold'
+      botEdt.style.fontSize = '1.3vw'
 
-    botEdt.innerHTML = '✏️'
-    botEdt.style.position = 'absolute'
-    botEdt.style.left = '38vw'
-    botEdt.style.background = 'w'
-    botEdt.style.borderRadius = '5px'
-    botEdt.style.color = 'black'
-    botEdt.style.fontWeight = 'bold'
-    botEdt.style.fontSize = '1.3vw'
+      op3.appendChild(botEdt)
 
-    op5.appendChild(botEdt)
+    } else if (op5.innerHTML == '' && op6.innerHTML == '' && op7.innerHTML == '' && op8.innerHTML == '') {
 
-  } else if (op7.innerHTML == '' && op8.innerHTML == '') {
+      op4.innerHTML = `- ${ativ.value}`
+      op4.style.marginTop = '7.4vw'
+      op5.innerHTML = '-'
+      ativ.value = ''
+      ativ.placeholder = 'Atividade'
+      ativ.focus
 
-    op6.innerHTML = `6 - ${ativ.value}`
-    op7.innerHTML = '7 '
-    ativ.value = ''
-    ativ.placeholder = 'Atividade'
-    ativ.focus
+      var botdel = document.createElement('button')
+      botdel.innerHTML = 'X'
 
-    var botdel = document.createElement('button')
-    botdel.innerHTML = 'X'
-    botdel.style.position = 'absolute'
-    botdel.style.left = '35vw'
-    botdel.style.background = 'red'
-    botdel.style.borderRadius = '5px'
-    botdel.style.color = 'black'
-    botdel.style.fontWeight = 'bold'
-    botdel.style.fontSize = '1.3vw'
+      botdel.style.position = 'absolute'
+      botdel.style.left = '35vw'
+      botdel.style.background = 'red'
+      botdel.style.borderRadius = '5px'
+      botdel.style.color = 'black'
+      botdel.style.fontWeight = 'bold'
+      botdel.style.fontSize = '1.3vw'
 
-    op6.appendChild(botdel)
+      botdel.addEventListener('click', function () {
+        op4.remove(op4)
+        op4d.remove(op4d)
+        botão.style.visibility = 'visible'
+        ativ.value = ''
+        ativ.placeholder = 'Atividade'
+        ativ.disabled = false
+        botão.disabled = false
+      })
 
-    botEdt.innerHTML = '✏️'
-    botEdt.style.position = 'absolute'
-    botEdt.style.left = '38vw'
-    botEdt.style.background = 'w'
-    botEdt.style.borderRadius = '5px'
-    botEdt.style.color = 'black'
-    botEdt.style.fontWeight = 'bold'
-    botEdt.style.fontSize = '1.3vw'
+      op4.appendChild(botdel)
 
-    op6.appendChild(botEdt)
+      botEdt.innerHTML = '✏️'
+      botEdt.style.position = 'absolute'
+      botEdt.style.left = '38vw'
+      botEdt.style.background = 'w'
+      botEdt.style.borderRadius = '5px'
+      botEdt.style.color = 'black'
+      botEdt.style.fontWeight = 'bold'
+      botEdt.style.fontSize = '1.3vw'
 
-  } else if (op8.innerHTML == '') {
+      op4.appendChild(botEdt)
 
-    op7.innerHTML = `7 - ${ativ.value}`
-    op8.innerHTML = '8'
-    ativ.value = ''
-    ativ.placeholder = 'Atividade'
-    ativ.focus
+    } else if (op6.innerHTML == '' && op7.innerHTML == '' && op8.innerHTML == '') {
 
-    var botdel = document.createElement('button')
-    botdel.innerHTML = 'X'
-    botdel.style.position = 'absolute'
-    botdel.style.left = '35vw'
-    botdel.style.background = 'red'
-    botdel.style.borderRadius = '5px'
-    botdel.style.color = 'black'
-    botdel.style.fontWeight = 'bold'
-    botdel.style.fontSize = '1.3vw'
+      op5.innerHTML = `- ${ativ.value}`
+      op5.style.marginTop = '9.6vw'
+      op6.innerHTML = '-'
+      ativ.value = ''
+      ativ.placeholder = 'Atividade'
+      ativ.focus
 
-    op7.appendChild(botdel)
+      var botdel = document.createElement('button')
+      botdel.innerHTML = 'X'
 
-    botEdt.innerHTML = '✏️'
-    botEdt.style.position = 'absolute'
-    botEdt.style.left = '38vw'
-    botEdt.style.background = 'w'
-    botEdt.style.borderRadius = '5px'
-    botEdt.style.color = 'black'
-    botEdt.style.fontWeight = 'bold'
-    botEdt.style.fontSize = '1.3vw'
 
-    op7.appendChild(botEdt)
+      botdel.style.position = 'absolute'
+      botdel.style.left = '35vw'
+      botdel.style.background = 'red'
+      botdel.style.borderRadius = '5px'
+      botdel.style.color = 'black'
+      botdel.style.fontWeight = 'bold'
+      botdel.style.fontSize = '1.3vw'
 
+      botdel.addEventListener('click', function () {
+        op5.remove(op5)
+        op5d.remove(op5d)
+        botão.style.visibility = 'visible'
+        ativ.value = ''
+        ativ.placeholder = 'Atividade'
+        ativ.disabled = false
+        botão.disabled = false
+      })
+
+      op5.appendChild(botdel)
+
+      botEdt.innerHTML = '✏️'
+      botEdt.style.position = 'absolute'
+      botEdt.style.left = '38vw'
+      botEdt.style.background = 'w'
+      botEdt.style.borderRadius = '5px'
+      botEdt.style.color = 'black'
+      botEdt.style.fontWeight = 'bold'
+      botEdt.style.fontSize = '1.3vw'
+
+      op5.appendChild(botEdt)
+
+    } else if (op7.innerHTML == '' && op8.innerHTML == '') {
+
+      op6.innerHTML = `- ${ativ.value}`
+      op6.style.marginTop = '11.6vw'
+      op7.innerHTML = '-'
+      ativ.value = ''
+      ativ.placeholder = 'Atividade'
+      ativ.focus
+
+      var botdel = document.createElement('button')
+      botdel.innerHTML = 'X'
+      botdel.style.position = 'absolute'
+      botdel.style.left = '35vw'
+      botdel.style.background = 'red'
+      botdel.style.borderRadius = '5px'
+      botdel.style.color = 'black'
+      botdel.style.fontWeight = 'bold'
+      botdel.style.fontSize = '1.3vw'
+
+      botdel.addEventListener('click', function () {
+        op6.remove(op6)
+        op6d.remove(op6d)
+        botão.style.visibility = 'visible'
+        ativ.value = ''
+        ativ.placeholder = 'Atividade'
+        ativ.disabled = false
+        botão.disabled = false
+      })
+
+      op6.appendChild(botdel)
+
+      botEdt.innerHTML = '✏️'
+      botEdt.style.position = 'absolute'
+      botEdt.style.left = '38vw'
+      botEdt.style.background = 'w'
+      botEdt.style.borderRadius = '5px'
+      botEdt.style.color = 'black'
+      botEdt.style.fontWeight = 'bold'
+      botEdt.style.fontSize = '1.3vw'
+
+      op6.appendChild(botEdt)
+
+    } else if (op8.innerHTML == '') {
+
+      op7.innerHTML = `- ${ativ.value}`
+      op7.style.marginTop = '13.8vw'
+      op8.innerHTML = '-'
+      ativ.value = ''
+      ativ.placeholder = 'Atividade'
+      ativ.focus
+
+      var botdel = document.createElement('button')
+      botdel.innerHTML = 'X'
+      botdel.style.position = 'absolute'
+      botdel.style.left = '35vw'
+      botdel.style.background = 'red'
+      botdel.style.borderRadius = '5px'
+      botdel.style.color = 'black'
+      botdel.style.fontWeight = 'bold'
+      botdel.style.fontSize = '1.3vw'
+
+      botdel.addEventListener('click', function () {
+        op7.remove(op7)
+        op7d.remove(op7d)
+        botão.style.visibility = 'visible'
+        ativ.value = ''
+        ativ.placeholder = 'Atividade'
+        ativ.disabled = false
+        botão.disabled = false
+      })
+
+      op7.appendChild(botdel)
+
+      botEdt.innerHTML = '✏️'
+      botEdt.style.position = 'absolute'
+      botEdt.style.left = '38vw'
+      botEdt.style.background = 'w'
+      botEdt.style.borderRadius = '5px'
+      botEdt.style.color = 'black'
+      botEdt.style.fontWeight = 'bold'
+      botEdt.style.fontSize = '1.3vw'
+
+      op7.appendChild(botEdt)
+
+    }
+
+
+    else {
+
+      op8.innerHTML = `- ${ativ.value}`
+      op8.style.marginTop = '16vw'
+      ativ.value = ''
+      ativ.placeholder = 'Atividade'
+      ativ.focus
+      botão.disabled = true
+      ativ.disabled = true
+
+      var botdel = document.createElement('button')
+      botdel.innerHTML = 'X'
+      botdel.style.position = 'absolute'
+      botdel.style.left = '35vw'
+      botdel.style.background = 'red'
+      botdel.style.borderRadius = '5px'
+      botdel.style.color = 'black'
+      botdel.style.fontWeight = 'bold'
+      botdel.style.fontSize = '1.3vw'
+
+      botdel.addEventListener('click', function () {
+        op8.remove(op8)
+        op8d.remove(op8d)
+        botão.style.visibility = 'visible'
+        ativ.value = ''
+        ativ.placeholder = 'Atividade'
+        ativ.disabled = false
+        botão.disabled = false
+      })
+
+      op8.appendChild(botdel)
+
+      botEdt.innerHTML = '✏️'
+      botEdt.style.position = 'absolute'
+      botEdt.style.left = '38vw'
+      botEdt.style.background = 'w'
+      botEdt.style.borderRadius = '5px'
+      botEdt.style.color = 'black'
+      botEdt.style.fontWeight = 'bold'
+      botEdt.style.fontSize = '1.3vw'
+
+      op8.appendChild(botEdt)
+
+    }
   }
-  
-  
-  else {
-
-    op8.innerHTML = `8 - ${ativ.value}`
-    ativ.value = ''
-    ativ.placeholder = 'Atividade'
-    ativ.focus
-    botão.disabled = true
-    ativ.disabled = true
-
-    var botdel = document.createElement('button')
-    botdel.innerHTML = 'X'
-    botdel.style.position = 'absolute'
-    botdel.style.left = '35vw'
-    botdel.style.background = 'red'
-    botdel.style.borderRadius = '5px'
-    botdel.style.color = 'black'
-    botdel.style.fontWeight = 'bold'
-    botdel.style.fontSize = '1.3vw'
-    op8.appendChild(botdel)
-
-    botEdt.innerHTML = '✏️'
-    botEdt.style.position = 'absolute'
-    botEdt.style.left = '38vw'
-    botEdt.style.background = 'w'
-    botEdt.style.borderRadius = '5px'
-    botEdt.style.color = 'black'
-    botEdt.style.fontWeight = 'bold'
-    botEdt.style.fontSize = '1.3vw'
-
-    op8.appendChild(botEdt)
-
-  }
-}
 }
 botão.addEventListener('click', adicionar)
 resetar.addEventListener('click', limpar)
 
-op1d.addEventListener('click', mudaCor)
-function mudaCor() {
-op1d.style.background = 'green'
-  }
+
